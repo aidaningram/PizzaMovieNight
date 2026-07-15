6f0c2793fe998a2aaea88bb4e764b189b6e33fcb
aidaningram <aidaningram.video@gmail.com>
2026-07-15 19:47:59 -0400

    Add Pizza Scale Firebase config option

---
+++ firebase-config.pizza-scale.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC32incQCNZAQ52hxCGa4mzRCpfIuoAsKI",
+  authDomain: "the-pizza-scale.firebaseapp.com",
+  databaseURL: "https://the-pizza-scale-default-rtdb.firebaseio.com",
+  projectId: "the-pizza-scale",
+  storageBucket: "the-pizza-scale.firebasestorage.app",
+  messagingSenderId: "805928480114",
+  appId: "1:805928480114:web:757451897522cf1def01eb",
+  measurementId: "G-DTER8KTXMF"
+};
